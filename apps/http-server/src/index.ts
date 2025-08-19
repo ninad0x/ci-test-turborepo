47280dec81db06e7a357ac9fd6f8966e9d7a82c0
Ninad09 <ninadgaonkar234@gmail.com>
2025-08-19 20:30:20 +0530

    completed basic app

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import { client } from "@repo/db/client"
+
+const app = express();
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.send("Hi there");
+})
+
+app.post("/signup", async (req, res) => {
+    const { username, password } = req.body;
+    
+    const user = await client.user.create({
+        data: {
+            username: username,
+            password: password
+        }
+    }) 
+
+    res.json({
+        message: "Signup successful",
+        id: user.id
+    })
+})
+
+
+app.listen(3002)
